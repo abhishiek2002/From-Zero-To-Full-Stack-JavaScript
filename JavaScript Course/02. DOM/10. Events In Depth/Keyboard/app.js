926c4f525ef2a96ccc96a52cb30e33c0ebee2820
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-31 19:12:27 +0530

    Keyboard Events

---
+++ JavaScript Course/02. DOM/10. Events In Depth/Keyboard/app.js
@@ -0,0 +1,34 @@
+// https://www.w3schools.com/jsref/obj_keyboardevent.asp
+
+const input = document.querySelector('input');
+
+// input.addEventListener('keypress', () => {
+//     console.log("key pressed");
+    
+// });
+
+// input.addEventListener('keyup', () => {
+//     console.log("key up");
+    
+// });
+
+// input.addEventListener('keydown', () => {
+//     console.log("key down");
+    
+// });
+
+// usefull properties and methods
+
+// input.addEventListener('keypress' , e =>{
+//     console.log(e.charCode);  // it give the character code of key that you pressed right now
+
+//     console.log(e.code);  // it will give the key name you press just now (eg:- KEYA, KEYB)
+
+//     console.log(e.ctrlKey);  // it will give true if key press with "ctrl" key otherwise false
+    
+//     console.log(e.shiftKey); // it wil give true if key press with "shift" key otherwise false
+
+//     console.log(e.key);  // it will give key that you press (a, b, c, 1, .....)
+    
+    
+// })
