584b646353b00ec4f2e029570fee92fee1395078
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-03-31 19:14:16 +0530

    Random Emojies Project

---
+++ JavaScript Course/02. DOM/11. Projects/1. Random Emojies/app.js
@@ -0,0 +1,28 @@
+const emoji = document.querySelector("#emoji")
+
+const emojis = [
+    "😊",
+    "🤣",
+    "❤️",
+    "💕",
+    "😘",
+    "😒",
+    "😁",
+    "😒",
+    "😊",
+    "😂",
+    "😍",
+    "😘",
+    "🤣",
+    "😜",
+    "😢",
+    "😎",
+    "😉",
+    "🙂",
+    "🤗",
+    "🥲"
+];
+
+emoji.addEventListener("mouseover", () =>{
+    emoji.innerHTML = emojis[Math.floor(Math.random()*emojis.length)];
+})
